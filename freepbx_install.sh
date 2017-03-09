bb3fb2dc5b20e2666fdaf6c2ead5a1f5fa89a1f6
Kory Wegmeyer <kewegmey@mtu.edu>
2017-03-09 17:24:22 -0500

    init
    init

---
+++ freepbx_install.sh
@@ -0,0 +1,155 @@
+#!/usr/bin/bash
+# Installs freepbx 13 on centos 7
+
+# :(
+sed -i 's/\(^SELINUX=\).*/\SELINUX=disabled/' /etc/sysconfig/selinux
+sed -i 's/\(^SELINUX=\).*/\SELINUX=disabled/' /etc/selinux/config
+
+# Update and get dev tools
+yum -y update
+yum -y groupinstall core base "Development Tools"
+
+# Other dependencies
+yum -y install mariadb-server mariadb php php-mysql php-mbstring \
+  httpd ncurses-devel sendmail sendmail-cf sox newt-devel libxml2-devel libtiff-devel \
+  audiofile-devel gtk2-devel subversion kernel-devel git php-process crontabs cronie \
+  cronie-anacron wget vim php-xml uuid-devel sqlite-devel net-tools gnutls-devel php-pear unixODBC mysql-connect
+or-odbc
+
+# Legacy Pear Reqs
+pear install Console_Getopt
+
+# Start and enable database
+systemctl enable mariadb.service
+systemctl start mariadb
+
+# Database basic setup
+SECURE_MYSQL=$(expect -c "
+set timeout 10
+spawn mysql_secure_installation
+expect \"Enter current password for root (enter for none):\"
+send \"\r\"
+expect \"Change the root password?\"
+send \"n\r\"
+expect \"Remove anonymous users?\"
+send \"y\r\"
+expect \"Disallow root login remotely?\"
+send \"y\r\"
+expect \"Remove test database and access to it?\"
+send \"y\r\"
+expect \"Reload privilege tables now?\"
+send \"y\r\"
+expect eof
+")
+
+echo "$SECURE_MYSQL"
+
+# Fire up apache
+systemctl enable httpd.service
+systemctl start httpd.service
+
+# Add asterisk user
+adduser asterisk -M -c "Asterisk User"
+
+# Download sources
+cd /usr/src
+wget http://downloads.asterisk.org/pub/telephony/libpri/libpri-current.tar.gz
+wget http://downloads.asterisk.org/pub/telephony/asterisk/asterisk-13-current.tar.gz
+wget -O jansson.tar.gz https://github.com/akheron/jansson/archive/v2.7.tar.gz
+wget http://www.pjsip.org/release/2.4/pjproject-2.4.tar.bz2
+# Compile and install pjproject
+cd /usr/src
+tar -xjvf pjproject-2.4.tar.bz2
+rm -f pjproject-2.4.tar.bz2
+cd pjproject-2.4
+CFLAGS='-DPJ_HAS_IPV6=1' ./configure --prefix=/usr --enable-shared --disable-sound\
+  --disable-resample --disable-video --disable-opencore-amr --libdir=/usr/lib64
+make dep
+make
+make install
+
+# Compile and Install jansson
+cd /usr/src
+tar vxfz jansson.tar.gz
+rm -f jansson.tar.gz
+cd jansson-*
+autoreconf -i
+./configure --libdir=/usr/lib64
+make
+make install 
+
+# Compile and Install Asterisk
+
+cd /usr/src
+tar xvfz asterisk-13-current.tar.gz
+rm -f asterisk-13-current.tar.gz
+cd asterisk-*
+contrib/scripts/install_prereq install
+./configure --libdir=/usr/lib64
+contrib/scripts/get_mp3_source.sh
+make menuselect
+
+make
+make install
+make config
+ldconfig
+systemctl disable asterisk
+
+# Soundfiles 
+cd /var/lib/asterisk/sounds
+wget http://downloads.asterisk.org/pub/telephony/sounds/asterisk-core-sounds-en-wav-current.tar.gz
+wget http://downloads.asterisk.org/pub/telephony/sounds/asterisk-extra-sounds-en-wav-current.tar.gz
+tar xvf asterisk-core-sounds-en-wav-current.tar.gz
+rm -f asterisk-core-sounds-en-wav-current.tar.gz
+tar xfz asterisk-extra-sounds-en-wav-current.tar.gz
+rm -f asterisk-extra-sounds-en-wav-current.tar.gz
+# Wideband Audio download
+wget http://downloads.asterisk.org/pub/telephony/sounds/asterisk-core-sounds-en-g722-current.tar.gz
+wget http://downloads.asterisk.org/pub/telephony/sounds/asterisk-extra-sounds-en-g722-current.tar.gz
+tar xfz asterisk-extra-sounds-en-g722-current.tar.gz
+rm -f asterisk-extra-sounds-en-g722-current.tar.gz
+tar xfz asterisk-core-sounds-en-g722-current.tar.gz
+rm -f asterisk-core-sounds-en-g722-current.tar.gz
+
+# Permissions
+chown asterisk. /var/run/asterisk
+chown -R asterisk. /etc/asterisk
+chown -R asterisk. /var/{lib,log,spool}/asterisk
+chown -R asterisk. /usr/lib64/asterisk
+chown -R asterisk. /var/www/
+
+# Tweak apache configs
+sed -i 's/\(^upload_max_filesize = \).*/\120M/' /etc/php.ini
+sed -i 's/^\(User\|Group\).*/\1 asterisk/' /etc/httpd/conf/httpd.conf
+sed -i 's/AllowOverride None/AllowOverride All/' /etc/httpd/conf/httpd.conf
+systemctl restart httpd.service
+
+# Download and install freepbx
+cd /usr/src
+wget http://mirror.freepbx.org/modules/packages/freepbx/freepbx-13.0-latest.tgz
+tar xfz freepbx-13.0-latest.tgz
+rm -f freepbx-13.0-latest.tgz
+cd freepbx
+./start_asterisk start
+./install -n
+
+# Dat service
+echo "
+[Unit]
+Description=FreePBX VoIP Server
+After=mariadb.service
+ 
+[Service]
+Type=oneshot
+RemainAfterExit=yes
+ExecStart=/usr/sbin/fwconsole start
+ExecStop=/usr/sbin/fwconsole stop
+ 
+[Install]
+WantedBy=multi-user.target" > /etc/systemd/system/freepbx.service
+
+systemctl enable freepbx.service
+systemctl start freepbx.service
+
+
+# done
