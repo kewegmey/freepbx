d86c2934878e7c1b27387659a35db41333d7d5c4
Kory Wegmeyer <kewegmey@mtu.edu>
2017-04-03 11:52:47 -0400

    Added more comments

--- freepbx_install.sh
+++ freepbx_install.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/bash
 # Installs freepbx 13 on centos 7
 
-# I'm really sorry about this....freepbx actually checks to see if selinux is enabled...working on a way to get it running w/o disabling'
+# Freepbx actually checks to see if selinux is enabled...working on a way to get it running w/o disabling'
+
+# Pause for user to understand this will disable selinux and restart their machine.  
 echo 'This script will now disable selinux and restart your machine.  Press any key to continue. Then rerun this script once your machine is back up. ctrl-c if you dont want that.'
 read -n 1 -s
 
+
+# If selinux is not disabled modify configs to disable then restart to take effect.  
+# When the system reboots and the user runs this script again this will be skipped.  
+# The freepbx install script checks selinux.
 if [[ $( getenforce ) != "Disabled" ]]; then
   sed -i 's/\(^SELINUX=\).*/\SELINUX=disabled/' /etc/sysconfig/selinux
   sed -i 's/\(^SELINUX=\).*/\SELINUX=disabled/' /etc/selinux/config
@@ -15,13 +21,13 @@ fi
 yum -y update
 yum -y groupinstall core base "Development Tools"
 
-# Other dependencies
+# Install other dependencies for asterisk and freepbx
 yum -y install mariadb-server mariadb php php-mysql php-mbstring \
   httpd ncurses-devel sendmail sendmail-cf sox newt-devel libxml2-devel libtiff-devel \
   audiofile-devel gtk2-devel subversion kernel-devel git php-process crontabs cronie \
   cronie-anacron wget vim php-xml uuid-devel sqlite-devel net-tools gnutls-devel php-pear unixODBC mysql-connector-odbc
 
-# Legacy Pear Reqs
+# Legacy Pear Reqs (see http://wiki.freepbx.org/display/FOP/Installing+FreePBX+13+on+CentOS+7)
 pear install Console_Getopt
 
 # Start and enable database
@@ -29,6 +35,8 @@ systemctl enable mariadb.service
 systemctl start mariadb
 
 # Database basic setup
+# The lazy route, I just call the mysql_secure_install and give it answers.
+# A better way may be to run the equivalent mysql commands.
 SECURE_MYSQL=$(expect -c "
 set timeout 10
 spawn mysql_secure_installation
@@ -57,12 +65,17 @@ systemctl start httpd.service
 adduser asterisk -M -c "Asterisk User"
 
 # Download sources
+
+# Download Asterisk sources
 cd /usr/src
 wget http://downloads.asterisk.org/pub/telephony/libpri/libpri-current.tar.gz
 wget http://downloads.asterisk.org/pub/telephony/asterisk/asterisk-13-current.tar.gz
 wget -O jansson.tar.gz https://github.com/akheron/jansson/archive/v2.7.tar.gz
 wget http://www.pjsip.org/release/2.4/pjproject-2.4.tar.bz2
+
+
 # Compile and install pjproject
+# This is the 'new' sip library and is nicer for most uses than chan_sip
 cd /usr/src
 tar -xjvf pjproject-2.4.tar.bz2
 rm -f pjproject-2.4.tar.bz2
@@ -74,6 +87,7 @@ make
 make install
 
 # Compile and Install jansson
+# Json library for C 
 cd /usr/src
 tar vxfz jansson.tar.gz
 rm -f jansson.tar.gz
@@ -84,7 +98,7 @@ make
 make install 
 
 # Compile and Install Asterisk
-
+# Open Source Communications Software - the core of freepbx, does all the work
 cd /usr/src
 tar xvfz asterisk-13-current.tar.gz
 rm -f asterisk-13-current.tar.gz
@@ -92,15 +106,18 @@ cd asterisk-*
 contrib/scripts/install_prereq install
 ./configure --libdir=/usr/lib64
 contrib/scripts/get_mp3_source.sh
+# menuselect for user customization
 make menuselect
 
 make
 make install
 make config
 ldconfig
+# Make sure asterisk is down since we will be managing it later with freepbx
 systemctl disable asterisk
 
-# Soundfiles 
+# Soundfiles
+# Prerecoreded voice files/sounds and g722 support for HD audio.
 cd /var/lib/asterisk/sounds
 wget http://downloads.asterisk.org/pub/telephony/sounds/asterisk-core-sounds-en-wav-current.tar.gz
 wget http://downloads.asterisk.org/pub/telephony/sounds/asterisk-extra-sounds-en-wav-current.tar.gz
@@ -117,6 +134,7 @@ tar xfz asterisk-core-sounds-en-g722-current.tar.gz
 rm -f asterisk-core-sounds-en-g722-current.tar.gz
 
 # Permissions
+# Make sure asterisk owns everyhting it needs to own.
 chown asterisk. /var/run/asterisk
 chown -R asterisk. /etc/asterisk
 chown -R asterisk. /var/{lib,log,spool}/asterisk
@@ -124,12 +142,14 @@ chown -R asterisk. /usr/lib64/asterisk
 chown -R asterisk. /var/www/
 
 # Tweak apache configs
+# Increase PHP upload size, run appache as the asterisk user, and change security setting. 
 sed -i 's/\(^upload_max_filesize = \).*/\120M/' /etc/php.ini
 sed -i 's/^\(User\|Group\).*/\1 asterisk/' /etc/httpd/conf/httpd.conf
 sed -i 's/AllowOverride None/AllowOverride All/' /etc/httpd/conf/httpd.conf
 systemctl restart httpd.service
 
 # Download and install freepbx
+# Freepbx is really just a PHP app that runs a bunch of commands in asterisk.  
 cd /usr/src
 wget http://mirror.freepbx.org/modules/packages/freepbx/freepbx-13.0-latest.tgz
 tar xfz freepbx-13.0-latest.tgz
@@ -139,6 +159,7 @@ cd freepbx
 ./install -n
 
 # Dat service
+# create systemd unit that manages the whole system.  
 echo "
 [Unit]
 Description=FreePBX VoIP Server
@@ -156,5 +177,7 @@ WantedBy=multi-user.target" > /etc/systemd/system/freepbx.service
 systemctl enable freepbx.service
 systemctl start freepbx.service
 
+echo "Freepbx install complete.  You may want to restart the asterisk core before trying to do anyhting.  OR just reboot your machine."  
 
 # done
+ 
