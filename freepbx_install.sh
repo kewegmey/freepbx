d5e7baf449a7bbedbc6e08053a1613e1449abc09
Kory Wegmeyer <kewegmey@mtu.edu>
2017-04-11 20:47:03 -0400

    added debugging

--- freepbx_install.sh
+++ freepbx_install.sh
@@ -3,31 +3,34 @@
 # Installs freepbx 13 on centos 7
 # Thanks to Andrew Nagy and the other editors of the freepbx wiki for creating the guide I followed to make this script.  
 # http://wiki.freepbx.org/display/FOP/Installing+FreePBX+13+on+CentOS+7
+# kewegmey@mtu.edu
 ##########################################################################################################################
+debug=0
 
-# Array of dependencies - removed a few from the guide that clearly were not needed.  
-deps=(mariadb-server mariadb php php-mysql php-mbstring httpd ncurses-devel sendmail sendmail-cf sox newt-devel libxml2-devel libtiff-devel audiofile-devel gtk2-devel subversion kernel-devel git php-process crontabs cronie cronie-anacron wget vim php-xml uuid-devel sqlite-devel net-tools gnutls-devel php-pear unixODBC mysql-connector-odbc)
-
-
-##########################################################################################################################
-# Pre-reqs
-##########################################################################################################################
 
 # Freepbx actually checks to see if selinux is enabled...working on a way to get it running w/o disabling'
-
-# Pause for user to understand this will disable selinux and restart their machine.  
-echo 'This script will now disable selinux and restart your machine.  Press any key to continue. Then rerun this script once your machine is back up. ctrl-c if you dont want that.'
-read -n 1 -s
-
 
 # If selinux is not disabled modify configs to disable then restart to take effect.  
 # When the system reboots and the user runs this script again this will be skipped.  
 # The freepbx install script checks selinux.
 if [[ $( getenforce ) != "Disabled" ]]; then
+  # Pause for user to understand this will disable selinux and restart their machine.  
+  echo 'This script will now disable selinux and restart your machine.  Press any key to continue. Then rerun this script once your machine is back up. ctrl-c if you dont want that.'
+  read -n 1 -s
+	
   sed -i 's/\(^SELINUX=\).*/\SELINUX=disabled/' /etc/sysconfig/selinux
   sed -i 's/\(^SELINUX=\).*/\SELINUX=disabled/' /etc/selinux/config
   reboot
 fi
+
+function install {
+
+# Array of dependencies - removed a few from the guide that clearly were not needed.  
+deps=(mariadb-server mariadb php php-mysql php-mbstring httpd ncurses-devel sendmail sendmail-cf sox newt-devel libxml2-devel libtiff-devel audiofile-devel gtk2-devel subversion kernel-devel git php-process crontabs cronie cronie-anacron wget vim php-xml uuid-devel sqlite-devel net-tools gnutls-devel php-pear unixODBC mysql-connector-odbc)
+
+echo "##########################################################################################################################"
+echo "# Pre-reqs"
+echo "##########################################################################################################################"
 
 # Update and get dev tools
 yum -y update
@@ -78,9 +81,9 @@ systemctl start httpd.service
 adduser asterisk -M -c "Asterisk User"
 
 
-##########################################################################################################################
-# Download Sources
-##########################################################################################################################
+echo "##########################################################################################################################"
+echo "# Download Sources"
+echo "##########################################################################################################################"
 
 
 # Download Asterisk sources
@@ -91,10 +94,10 @@ wget -O jansson.tar.gz https://github.com/akheron/jansson/archive/v2.7.tar.gz
 wget http://www.pjsip.org/release/2.4/pjproject-2.4.tar.bz2
 
 
-##########################################################################################################################
-# Compile and install pjproject
-# This is the 'new' sip library and is nicer for most uses than chan_sip
-##########################################################################################################################
+echo "##########################################################################################################################"
+echo "# Compile and install pjproject"
+echo "# This is the 'new' sip library and is nicer for most uses than chan_sip"
+echo "##########################################################################################################################"
 cd /usr/src
 tar -xjvf pjproject-2.4.tar.bz2
 rm -f pjproject-2.4.tar.bz2
@@ -105,12 +108,12 @@ make dep
 make
 make install
 
-##########################################################################################################################
-# Compile and Install jansson
-# Json library for C 
-##########################################################################################################################
+echo "##########################################################################################################################"
+echo "# Compile and Install jansson"
+echo "# Json library for C"
+echo "##########################################################################################################################"
 cd /usr/src
-tar vxfz jansson.tar.gz
+tar xfz jansson.tar.gz
 rm -f jansson.tar.gz
 cd jansson-*
 autoreconf -i
@@ -118,10 +121,10 @@ autoreconf -i
 make
 make install 
 
-##########################################################################################################################
-# Compile and Install Asterisk
-# Open Source Communications Software - the core of freepbx, does all the work
-##########################################################################################################################
+echo "##########################################################################################################################"
+echo "# Compile and Install Asterisk"
+echo "# Open Source Communications Software - the core of freepbx, does all the work"
+echo "##########################################################################################################################"
 cd /usr/src
 tar xvfz asterisk-13-current.tar.gz
 rm -f asterisk-13-current.tar.gz
@@ -129,8 +132,6 @@ cd asterisk-*
 contrib/scripts/install_prereq install
 ./configure --libdir=/usr/lib64
 contrib/scripts/get_mp3_source.sh
-# menuselect for user customization
-make menuselect
 
 make
 make install
@@ -139,10 +140,10 @@ ldconfig
 # Make sure asterisk is down since we will be managing it later with freepbx
 systemctl disable asterisk
 
-##########################################################################################################################
-# Soundfiles
-# Prerecoreded voice files/sounds and g722 support for HD audio.
-##########################################################################################################################
+echo "##########################################################################################################################"
+echo "# Soundfiles"
+echo "# Prerecoreded voice files/sounds and g722 support for HD audio."
+echo "##########################################################################################################################"
 cd /var/lib/asterisk/sounds
 wget http://downloads.asterisk.org/pub/telephony/sounds/asterisk-core-sounds-en-wav-current.tar.gz
 wget http://downloads.asterisk.org/pub/telephony/sounds/asterisk-extra-sounds-en-wav-current.tar.gz
@@ -158,9 +159,9 @@ rm -f asterisk-extra-sounds-en-g722-current.tar.gz
 tar xfz asterisk-core-sounds-en-g722-current.tar.gz
 rm -f asterisk-core-sounds-en-g722-current.tar.gz
 
-##########################################################################################################################
-# Asterisk install cleanup
-##########################################################################################################################
+echo "##########################################################################################################################"
+echo "# Asterisk install cleanup"
+echo "##########################################################################################################################"
 # Permissions
 # Make sure asterisk owns everyhting it needs to own.
 chown asterisk. /var/run/asterisk
@@ -177,10 +178,10 @@ sed -i 's/AllowOverride None/AllowOverride All/' /etc/httpd/conf/httpd.conf
 systemctl restart httpd.service
 
 
-##########################################################################################################################
-# Download and install freepbx
-# Freepbx is really just a PHP app that runs a bunch of commands in asterisk.  
-##########################################################################################################################
+echo "##########################################################################################################################"
+echo "# Download and install freepbx"
+echo "# Freepbx is really just a PHP app that runs a bunch of commands in asterisk.  "
+echo "##########################################################################################################################"
 cd /usr/src
 wget http://mirror.freepbx.org/modules/packages/freepbx/freepbx-13.0-latest.tgz
 tar xfz freepbx-13.0-latest.tgz
@@ -208,7 +209,16 @@ WantedBy=multi-user.target" > /etc/systemd/system/freepbx.service
 systemctl enable freepbx.service
 systemctl start freepbx.service
 
+
 echo "Freepbx install complete.  You may want to restart the asterisk core before trying to do anyhting.  OR just reboot your machine."  
 
 # done
- 
+
+} # end install function
+
+
+if [[ $debug ]]; then
+  install > /dev/null
+else
+  install  	
+fi
